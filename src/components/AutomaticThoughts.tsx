32ce12b22b8df9f567f3a1c890339db82d0ade0e
Jon <jprather.dev@gmail.com>
2023-03-07 15:06:02 -0800

    removed cruft

--- src/components/AutomaticThoughts.tsx
+++ src/components/AutomaticThoughts.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import type { RefObject } from "react";
 import type { CBT_FormDataType } from "../types/CBTFormTypes";
-import { FaPlusCircle, FaTrash, FaFire, FaCircle } from "react-icons/fa";
-import { MdLocalFireDepartment, MdSentimentNeutral } from "react-icons/md";
+import { FaPlusCircle, FaTrash, FaFire } from "react-icons/fa";
 import type { ChangeEventHandler } from "react";
 import Modal from "./molecules/Modal";
 import DialogDemo from "./RadixDialog";
@@ -11,15 +10,12 @@ export const ANTDataAtributes = {
   addAntLabel: "addAntLabel",
   addAntInput: "addAntInput",
   addAntBtn: "addAntBtn",
-
   toggleHotAntBtn: "toggleHotAntBtn",
-
   deleteAntBtn: "deleteAntBtn",
 };
 type AutomaticThoughtsProps = {
   data: CBT_FormDataType;
   currentStep: number;
-  errors: any;
   handleChange: ChangeEventHandler<HTMLInputElement>;
   setData: React.Dispatch<React.SetStateAction<CBT_FormDataType>>;
 };
@@ -27,35 +23,12 @@ type AutomaticThoughtsProps = {
 const AutomaticThoughts: React.FC<AutomaticThoughtsProps> = ({
   data,
   currentStep,
-  errors,
   setData,
   handleChange,
 }) => {
-  const handleDelete = (index: number) => {
-    setData((data) => {
-      const newAutomaticThoughts = data.automaticThoughts.filter(
-        (thought, i) => i !== index
-      );
-      console.log("NEW ANTS AFTER", newAutomaticThoughts);
+  const textInput: RefObject<HTMLInputElement> = React.createRef();
 
-      return { ...data, automaticThoughts: newAutomaticThoughts };
-    });
-  };
-  console.log("DATA GLOBAL", data);
   const handleHotThoughtClick = (index: number) => {
-    console.log(
-      "ANT LENGTH",
-      data.automaticThoughts.filter((ele) => ele?.isHot).length > 0
-    );
-    if (
-      data.automaticThoughts.filter((ele) => ele?.isHot).length > 0 &&
-      !data?.automaticThoughts[index]?.isHot
-    ) {
-      // return alert("Already an ANT please choose one!");
-      // not sure if i want thsi for the userExperience...
-    }
-    // ok only want this to block if it is a different ant then the current hot though
-
     setData((prev) => {
       const newThoughts = [...prev.automaticThoughts];
       newThoughts[index] = {
@@ -69,6 +42,16 @@ const AutomaticThoughts: React.FC<AutomaticThoughtsProps> = ({
       };
     });
   };
+
+  const handleDelete = (index: number) => {
+    setData((data) => {
+      const newAutomaticThoughts = data.automaticThoughts.filter(
+        (thought, i) => i !== index
+      );
+      return { ...data, automaticThoughts: newAutomaticThoughts };
+    });
+  };
+
   const handleClick = () => {
     const newThought = textInput?.current?.value || "";
     if (!newThought) return;
@@ -88,16 +71,10 @@ const AutomaticThoughts: React.FC<AutomaticThoughtsProps> = ({
   };
 
   const handleKeyDown = (event: React.KeyboardEvent) => {
-    console.log("IN HANDLEKEY", event, event.key);
     if (event.code === "Enter") {
-      console.log("IN HANDLEKEY--inner");
-
       handleClick();
     }
   };
-  // TODO on update route this gets overwritten as it pulls from db and this isnt saved...
-  // so need to slow down the update pulling, and also consider auto saves...
-  const textInput: RefObject<HTMLInputElement> = React.createRef();
 
   if (currentStep !== 1) return null;
   return (
@@ -147,8 +124,7 @@ const AutomaticThoughts: React.FC<AutomaticThoughtsProps> = ({
         </div>
       </div>
 
-      {/*  */}
-      <div className={" "}>
+      <div>
         <div className="flex items-end justify-between">
           <label className="label">
             <Modal
@@ -161,12 +137,9 @@ const AutomaticThoughts: React.FC<AutomaticThoughtsProps> = ({
             />
           </label>
         </div>
-        {/* Might be cool to add ability to remove a ANT ie undo */}
-        {/* TODO fix undo its not deleting first elemnt and delteing wrong element at times... */}
+
         <div className=" over h-48 w-full overflow-y-auto overscroll-contain rounded-lg bg-slate-300 p-2 ">
           <ul>
-            {/* Well apparently yeah its needed could  */}
-            {/* This sort of advice could be conditional on being a new user */}
             {!data?.automaticThoughts[0] && (
               <li className="relative mb-2 rounded bg-gray-100 p-2">
                 <span className="p-2 italic text-gray-800">
@@ -179,25 +152,23 @@ const AutomaticThoughts: React.FC<AutomaticThoughtsProps> = ({
             {data?.automaticThoughts.map((thoughts, index) => (
               <li
                 key={index}
-                className={`relative mb-2 block min-h-[100%] w-[100%] overflow-clip overflow-ellipsis rounded-xl bg-white p-2 pr-0 ${
+                className={`relative mb-2 block min-h-[100%] w-[100%] overflow-clip overflow-ellipsis rounded-xl bg-white p-1 pr-0 ${
                   thoughts?.isHot ? " border-red-500 bg-secondary " : ""
                 }`}
               >
                 <button
                   data-testid={ANTDataAtributes.toggleHotAntBtn}
-                  className={`text-md mr-4 flex w-[90%]  cursor-pointer items-center p-2 p-1 text-left ${
+                  className={`text-md mr-4 flex w-[90%]  cursor-pointer items-center p-2 text-left ${
                     thoughts?.isHot ? "text-gray-900" : "text-gray-900"
                   }`}
                   onClick={() => handleHotThoughtClick(index)}
                 >
-                  <span className="absolute top-3 left-4   ">
-                    {/* TODO HMMM this extra space looks jank on modal how can i make this even? */}
-                    {/* IDK nto totally into the icons placement its close maybe good enough for abs pos */}
-                    {thoughts?.isHot ? (
-                      <FaFire className="min-h-[24px] text-sm text-accent" />
-                    ) : (
-                      <FaFire className="min-h-[24px] text-sm text-gray-300" />
-                    )}
+                  <span className="absolute top-3 left-4">
+                    <FaFire
+                      className={`min-h-[24px] text-sm ${
+                        thoughts?.isHot ? "text-accent" : "text-gray-300"
+                      } `}
+                    />
                   </span>
                   <span className="text-md ml-8 mr-4 overflow-clip xs:mr-2">
                     {thoughts?.thought}
@@ -222,10 +193,6 @@ const AutomaticThoughts: React.FC<AutomaticThoughtsProps> = ({
             ))}
           </ul>
         </div>
-
-        {/* {errors?.automaticThoughts && (
-          <div className="text-red-500">{errors.automaticThoughts}</div>
-        )} */}
       </div>
     </>
   );
