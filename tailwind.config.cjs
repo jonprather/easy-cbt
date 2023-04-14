200886b55bbbdc8ecf107b73fbdcb09a7bf91222
Jon <jprather.dev@gmail.com>
2023-04-14 16:43:44 -0700

    added shadcn deps and primitives

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+// module.exports = {
+//   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     screens: {
+//       six: "375px",
+//       xs: "475px",
+//       ...defaultTheme.screens,
+//     },
+//     extend: {},
+//   },
+//   plugins: [require("daisyui")],
+// };
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class", '[data-theme="dark"]'],
+  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
   theme: {
     screens: {
       six: "375px",
       xs: "475px",
       ...defaultTheme.screens,
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("tailwindcss-animate", "daisyui")],
 };
+
+// ok i went thru sahdcn steps now shits broke...
+// wish i would have committed before doing so
+// the problem is adding the second one it breaks my app
